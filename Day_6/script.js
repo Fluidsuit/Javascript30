5665cfb8a1df6ba66bd38067b55e1ed64b2db8df
Fluidsuit <mehertank1911@gmail.com>
2024-09-29 00:30:23 +0530

    .

---
+++ Day_6/script.js
@@ -0,0 +1,9 @@
+let num= 25;
+
+let input =  prompt("Guess the number");
+while(num != input)
+{
+    input= prompt("Your Guess is wrong, Guess another number")
+    console.log("opssss   wrong guess")
+}
+console.log("congratulation you guess the right number")
---
+++ Day_7/style.js
@@ -0,0 +1,3 @@
+let name= prompt("Enter your name to generate the username");
+username= '@'+ name + name.length;
+console.log("Your username is ", username);
